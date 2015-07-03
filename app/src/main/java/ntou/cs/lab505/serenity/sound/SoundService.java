572928fc626459bac1eaa81b79012585a68515f6
alantitor <belosianau@gmail.com>
2015-07-03 15:01:25 +0800

    sound manager class

---
+++ app/src/main/java/ntou/cs/lab505/serenity/sound/SoundService.java
@@ -0,0 +1,7 @@
+package ntou.cs.lab505.serenity.sound;
+
+/**
+ * Created by alan on 2015/7/3.
+ */
+public class SoundService {
+}
---
+++ app/src/main/java/ntou/cs/lab505/serenity/sound/SoundTool.java
@@ -0,0 +1,131 @@
+package ntou.cs.lab505.serenity.sound;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+import ntou.cs.lab505.serenity.datastructure.SoundVectorUnit;
+
+
+/**
+ * Created by alan on 6/12/15.
+ */
+public class SoundTool {
+
+    /**
+     * merge two one-channel sound vectors into a two-channels sound vector.
+     * @param leftChannel
+     * @param rightChannel
+     * @return
+     */
+    public static short[] channelTwo2One(short[] leftChannel, short[] rightChannel) {
+
+        short[] outputVector = null;
+
+        if (leftChannel != null && rightChannel != null) {  // we have two channel data.
+            Log.d("SoundTool", "in channelTwo2One. I am here 1.");
+            outputVector = new short[leftChannel.length * 2];
+            // data: LRLRLRLR...
+            for (int i = 0; i < leftChannel.length; i++) {
+                outputVector[i * 2] = leftChannel[i];
+                outputVector[i * 2 + 1] = rightChannel[i];
+            }
+        } else if (leftChannel == null && rightChannel != null){  // we only have right channel data. set zero to left channel.
+            Log.d("SoundTool", "in channelTwo2One. I am here 2.");
+            outputVector = new short[rightChannel.length * 2];
+            // data: 0R0R0R0R...
+            for (int i = 0; i < rightChannel.length; i++) {
+                outputVector[i * 2] = 0;
+                outputVector[i * 2 + 1] = rightChannel[i];
+            }
+        } else if (leftChannel != null && rightChannel == null){  // we only have left channel data. set zero to right channel.
+            Log.d("SoundTool", "in channelTwo2One. I am here 3.");
+            outputVector = new short[leftChannel.length * 2];
+            // data: L0L0L0L0...
+            for (int i = 0; i < leftChannel.length; i++) {
+                outputVector[i * 2] = leftChannel[i];
+                outputVector[i * 2 + 1] = 0;
+            }
+        } else {
+            Log.d("SoundTool", "in channelTwo2One. I am here 4.");
+            outputVector = new short[2];
+            outputVector[0] = 0;
+            outputVector[1] = 0;
+        }
+
+        return outputVector;
+    }
+
+    public static short[] channelMix(ArrayList<short[]> soundBands) {
+        short[] tempVector = new short[soundBands.get(0).length];
+        int temp = 0;
+
+        for (int trace = 0; trace < soundBands.get(0).length; trace++) {
+            for (int i = 0; i < soundBands.size(); i++) {
+                temp += soundBands.get(i)[trace];
+            }
+
+            tempVector[trace] = (short) temp;
+            temp = 0;
+        }
+
+        return tempVector;
+    }
+
+    /*
+     * 計算音量
+     * data - 欲計算的資料
+     * return 音量
+     */
+    public static int calculateDb(short[] data) {
+        double sum = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            sum += Math.pow(data[i], 2);
+        }
+        sum = 10 * Math.log10(sum / data.length);
+
+        return (int)sum;
+    }
+
+    public static double calculateDbDouble(short[] data) {
+        double sum = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            sum += Math.pow(data[i], 2);
+        }
+        sum = 10 * Math.log10(sum / data.length);
+
+        return sum;
+    }
+
+    public static void saveVectorToDataFile(short[] data, String fileName) {
+        File file = new File(Environment.getExternalStorageDirectory().toString() + "/Download/" + fileName + ".txt");
+        FileOutputStream fOut;
+        OutputStreamWriter fWriter;
+
+        if (data == null) {
+            return ;
+        }
+
+        try {
+            file.createNewFile();
+            fOut = new FileOutputStream(file);
+            fWriter = new OutputStreamWriter(fOut);
+            for (int i = 0; i < data.length; i++) {
+                fWriter.append(data[i] + ",");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static double mag2db(double value) {
+        return Math.pow(10, value / 20);
+    }
+}
