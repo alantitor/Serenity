cbdf9edc8d80a99d84d288b73435a032e4b8ae51
alantitor <belosianau@gmail.com>
2015-07-03 13:57:42 +0800

    sound device manager

---
+++ app/src/main/java/ntou/cs/lab505/serenity/stream/SoundInputPool.java
@@ -0,0 +1,7 @@
+package ntou.cs.lab505.serenity.stream.device;
+
+/**
+ * Created by alan on 2015/7/3.
+ */
+public class SoundInputPool {
+}
---
+++ app/src/main/java/ntou/cs/lab505/serenity/stream/SoundOutputPool.java
@@ -0,0 +1,7 @@
+package ntou.cs.lab505.serenity.stream;
+
+/**
+ * Created by alan on 2015/7/3.
+ */
+public class SoundOutputPool {
+}
