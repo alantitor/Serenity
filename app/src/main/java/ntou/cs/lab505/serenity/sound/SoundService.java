ffc478061d334b90d687142a30d6c7a0c9fb559c
alantitor <belosianau@gmail.com>
2015-07-10 00:16:52 +0800

    update project

--- app/src/main/java/ntou/cs/lab505/serenity/sound/SoundService.java
+++ app/src/main/java/ntou/cs/lab505/serenity/sound/SoundService.java
@@ -110,6 +110,7 @@ public class SoundService extends Service {
         soundAllThread.setPriority(Thread.MAX_PRIORITY);
         soundAllThread.threadStart();
 
+
         //soundProcessThread = new SoundProcessThread();
         //soundProcessThread.setSoundInputQueue(soundQueue1);
         //soundProcessThread.setSoundOutputQueue(soundQueue2);
@@ -117,68 +118,11 @@ public class SoundService extends Service {
         //soundProcessThread.threadStart();
 
 
-
-
         //soundIOThread = new SoundIOThread();
         //soundIOThread.setSoundInputQueue(soundQueue1);
         //soundIOThread.setSoundOutputQueue(soundQueue2);
         //soundIOThread.setPriority(Thread.MAX_PRIORITY);
         //soundIOThread.threadStart();
-
-
-
-        /*
-        while (true) {
-            //soundIOThread.setPriority(Thread.MAX_PRIORITY);
-            Log.d("SoundService", "thread priority: " + soundIOThread.getPriority());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        */
-
-
-        /*
-        SoundInputPool soundInputPool = new SoundInputPool(8000, 0);
-        soundInputPool.open();
-        SoundOutputPool soundOutputPool = new SoundOutputPool(8000, 1, 2, 0);
-        soundOutputPool.open();
-
-        FrequencyShift frequencyShift = new FrequencyShift(8000, 1, 1, 0, 0);
-
-        BandGain bandGain = new BandGain(8000, 200, 3000, 5, 5, 5);
-
-
-
-        while (serviceState) {
-            //Log.d("SoundService", "debug: data: " + soundInputPool.read().getVectorLength());
-            long timems0 = System.currentTimeMillis();
-            long time0 = System.nanoTime();
-
-            SoundVectorUnit data = soundInputPool.read();
-            long timems1 = System.currentTimeMillis();
-            long time1 = System.nanoTime();
-            Log.d("SoundService", "debug: time1: " + (time1 - time0) / 1000000.0 + " " + (timems1 - timems0));
-
-            data = frequencyShift.process(data);
-            long timems2 = System.currentTimeMillis();
-            long time2 = System.nanoTime();
-            Log.d("SoundService", "debug: time2: " + (time2 - time1) / 1000000.0 + " " + (timems2 - timems1));
-
-            //data = bandGain.process(data);
-            long timems3 = System.currentTimeMillis();
-            long time3 = System.nanoTime();
-            Log.d("SoundService", "debug: time3: " + (time3 - time2) / 1000000.0 + " " + (timems3 - timems2));
-
-            soundOutputPool.write(data);
-            long timems4 = System.currentTimeMillis();
-            long time4 = System.nanoTime();
-            Log.d("SoundService", "debug: time4: " + (time4 - time0) / 1000000.0 + " " + (timems4 - timems0));
-            Log.d("SoundService", "debug: total time: " + (time4 - time0) / 1000000.0 + " " + (timems4 - timems0));
-        }
-        */
     }
 
     public void serviceStop() {
