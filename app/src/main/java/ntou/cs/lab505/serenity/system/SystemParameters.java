c0533168e06877ee75458c0a086c47983ce9accb
alantitor <belosianau@gmail.com>
2015-07-15 15:12:16 +0800

    define system parameters

---
+++ app/src/main/java/ntou/cs/lab505/serenity/system/SystemParameters.java
@@ -0,0 +1,10 @@
+package ntou.cs.lab505.serenity.system;
+
+/**
+ * Created by alan on 2015/7/11.
+ */
+public class SystemParameters {
+
+    public static final int SAMPLERATE_LOW = 16000;
+    public static final int SAMPLERATE_HIGH = 44100;
+}
