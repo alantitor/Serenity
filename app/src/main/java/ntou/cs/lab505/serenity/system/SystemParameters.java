007f932c51d7abe3821eb4cdff7875a78efc5f80
alantitor <belosianau@gmail.com>
2015-07-16 17:20:22 +0800

    add new value.

--- app/src/main/java/ntou/cs/lab505/serenity/system/SystemParameters.java
+++ app/src/main/java/ntou/cs/lab505/serenity/system/SystemParameters.java
@@ -5,6 +5,7 @@ package ntou.cs.lab505.serenity.system;
  */
 public class SystemParameters {
 
-    public static final int SAMPLERATE_LOW = 16000;
-    public static final int SAMPLERATE_HIGH = 44100;
+    public static final int SAMPLERATE_LOW = 8000;
+    public static final int SAMPLERATE_HIGH = 16000;
+    public static final int SAMPLERATE_VERYHIGH = 44100;
 }
